1eda8604641bcba2208398e9b580e213f3797858
unknown <emily_chhun23@yaoo.com>
2021-02-24 13:29:29 -0800

    message

--- src/template.js
+++ src/template.js
@@ -3,58 +3,58 @@ const buildTeam = Team => {
    
     const BuildManager = manager  => {
         return `
-        <div class="card col employee-card" style="width: 18rem;">
-        <div class="card-body card-header">
+        <div class="col-3 p-3 mb-2 bg-dark text-white">
+        <div class="card-body p-3 mb-2 bg-info text-white text-center">
         <h2 class="card-title">${manager.pushname()}</h2>
         <h3 class="card-title"><h3><i class="fas fa-mug-hot"></i>${manager.pushrole()}</h3>
         </div>
         <div class = "card-body">
         <ul class ="list-group list-group-flush">
-        <li class ="list-group-item">Employee ID: ${manager.pushid()}</li>
-        <li class ="list-group-item">Email: <a href="mailto:${manager.pushemail()}">${manager.pushemail()}</a></li>
-       <li class ="list-group-item">Office Number: ${manager.pushofficeNumber()}</li>
+        <li class ="list-group-item list-group-item-warning">Employee ID: ${manager.pushid()}</li>
+        <li class ="list-group-item list-group-item-warning">Email: <a href="mailto:${manager.pushemail()}">${manager.pushemail()}</a></li>
+       <li class ="list-group-item list-group-item-warning">Office Number: ${manager.pushofficeNumber()}</li>
         </ul>
         </div>
         </div>
-        `;
+        `
     } ;
 
     const BuildEngineer = engineer => {
         return `
-        <div class="card col employee-card" style="width: 18rem;">
-        <div class="card-body card-header">
+        <div class="col-3 p-3 mb-2 bg-dark text-white">
+        <div class="card-body p-3 mb-2 bg-info text-white text-center">
         <h2 class="card-title">${engineer.pushname()}</h2>
         <h3 class="card-title"><h3><i class="fas fa-glasses"></i>${engineer.pushrole()}</h3>
         </div>
         <div class = "card-body">
         <ul class ="list-group list-group-flush">
-        <li class ="list-group-item">Employee ID: ${engineer.pushid()}</li>
-        <li class ="list-group-item">Email: <a href = "mailto:${engineer.pushemail()}">${engineer.pushemail()}</a></li>
-        <li class ="list-group-item">Github: <a href="https:github.com">${engineer.pushgithub()}</a></li>
+        <li class ="list-group-item list-group-item-warning">Employee ID: ${engineer.pushid()}</li>
+        <li class ="list-group-item list-group-item-warning">Email: <a href = "mailto:${engineer.pushemail()}">${engineer.pushemail()}</a></li>
+        <li class ="list-group-item list-group-item-warning">Github: <a href="https:github.com">${engineer.pushgithub()}</a></li>
         </ul>
         </div>
         </div>
-        `;
+        `
        
     };
 
     const BuildIntern = intern => {
         console.log(intern)
         return `
-        <div class="card col employee-card" style="width: 18rem;">
-        <div class="card-body card-header">
+        <div class="col-3 p-3 mb-2 bg-dark text-white">
+        <div class="card-body p-3 mb-2 bg-info text-white text-center">
         <h2 class="card-title">${intern.pushname()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate"></i>${intern.pushrole()}</h3>
         </div>
         <div class = "card-body">
         <ul class ="list-group list-group-flush">
-        <li class ="list-group-item">Employee ID: ${intern.pushid()}</li>
-        <li class ="list-group-item">Email: <a href = "mailto:${intern.pushemail()}">${intern.pushemail()}</a></li>
-        <li class ="list-group-item">School: ${intern.pushschool()}</li>
+        <li class ="list-group-item list-group-item-warning">Employee ID: ${intern.pushid()}</li>
+        <li class ="list-group-item list-group-item-warning">Email: <a href = "mailto:${intern.pushemail()}">${intern.pushemail()}</a></li>
+        <li class ="list-group-item list-group-item-warning">School: ${intern.pushschool()}</li>
         </ul>
         </div>
         </div>
-     `;
+     `
     };
 
 
@@ -92,19 +92,17 @@ module.exports = Team => {
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+   
+            <div class="col-12 jumbotron">
+                <h1 class="text-center text-light bg-dark">My Team</h1>
             </div>
-        </div>
-    </div>
+    
     <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
+        <div class="row justify-content-center">
+          
                 ${buildTeam(Team)}
             </div>
-        </div>
+       
     </div>
 </body>
 </html>
